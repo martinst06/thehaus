3bb8ca2350a8753d6d589655b3e2eae95698389e
martinst06 <mstoyanov@adobe.com>
2025-03-13 11:11:56 +0100

    feat: core values

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="flex-1 flex flex-col items-center bg-[var(--haus-white)]">
+    <main className="flex-1 flex flex-col items-center bg-[var(--haus-white)] m-0 p-0">
       
       {/* Full Viewport Section */}
       <div className="flex h-[calc(100vh-80px)] w-full">
@@ -22,8 +22,21 @@ export default function Home() {
         </div>
       </div>
 
+      <div></div>
+
+      {/* NEW SECTION */}
+      <div className="w-full bg-white p-6 text-black">
+        <h1 className="text-7xl font-light text-center pt-40">CORE VALUES</h1>
+        <div className="flex justify-between font-thin mt-4 pt-20 pb-40 pl-15 pr-15">
+          <h1 className="text-3xl">ELEGANCE</h1>
+          <h1 className="text-3xl">COMMUNITY</h1>
+          <h1 className="text-3xl">CREATIVITY</h1>
+          <h1 className="text-3xl">QUALITY</h1>
+        </div>
+      </div>
+      
       {/* MISSION STATEMENT Section */}
-      <div className="w-full grid grid-cols-1 md:grid-cols-2 items-center mt-70 bg-[var(--haus-black)]">
+      <div className="w-full grid grid-cols-1 md:grid-cols-2 items-center bg-[var(--haus-black)]">
         <div className="order-2 md:order-1 px-8 py-6 text-white">
           <h3 className="text-2xl font-medium mb-4 border-b border-[var(--foreground)] pb-2">MISSION STATEMENT</h3>
           <p className="text-lg font-light leading-relaxed">
@@ -51,3 +64,5 @@ export default function Home() {
     </main>
   );
 }
+
+{/* <h1 className="text-7xl font-light text-black">CORE VALUES</h1> */}
