290b72963bdb57465d54edfdf57fe8627c84d8b5
martinst06 <mstoyanov@adobe.com>
2025-03-14 14:52:59 +0100

    feat: sub brands with pictures etc.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -80,18 +80,45 @@ export default function Home() {
         </div>
       </div>
 
-      <div className="w-full bg-[var(--haus-black)] p-6 text-white">
-        <div className="flex justify-between font-thin mt-4">
-          <div className="w-1/2 pl-20  pt-20">
-            <h1 className="text-5xl ml-auto mr-auto pb-6">SUB</h1>
-            <h1 className="text-5xl ml-auto mr-auto pb-6">BRANDS</h1>
+      <div className="w-full bg-[var(--haus-black)] p-8 text-white">
+        <div className="flex justify-between mt-4 mb-4 border-b border-[var(--foreground)]">
+          <div className="w-1/2 pl-20 pt-15">
+            <h1 className="text-6xl pb-6">SUB</h1>
+            <h1 className="text-6xl pb-20 font-thin">BRANDS</h1>
           </div>
-          <div className="w-1/2 pt-14">
+          <div className="w-1/2 pt-15 font-thin">
             <h1 className="text-l pt-1">The Haus Bazaar: Pre-loved designer resale events.</h1>
             <h1 className="text-l pt-1">The Haus Design: Bespoke interior design services.</h1>
             <h1 className="text-l pt-1">The Haus Events: High-end event planning and execution.</h1>
             <h1 className="text-l pt-1">The Haus Social: Luxury content creation and digital strategy</h1>
             <h1 className="text-l pt-1">The Haus Society: An exclusive community with access to unique experiences.</h1>
+          </div>
+        </div>
+        <div className="flex font-thin mb-4 mt-10 justify-between">
+          <div className="flex flex-col">
+            <Image src="/images/image00005.jpeg" alt="Image 1" width={200} height={200} className="object-cover h-50 w-50"/>
+            <h1 className="text-xl mt-2 font-bold">THE HAUS</h1>
+            <h1 className="text-s">BAZAAR</h1>
+          </div>
+          <div className="flex flex-col">
+            <Image src="/images/image00005.jpeg" alt="Image 2" width={200} height={200} className="object-cover h-50 w-50"/>
+            <h1 className="text-xl mt-2 font-bold">THE HAUS</h1>
+            <h1 className="text-s">DESIGN</h1>
+          </div>
+          <div className="flex flex-col">
+            <Image src="/images/image00005.jpeg" alt="Image 3" width={200} height={200} className="object-cover h-50 w-50"/>
+            <h1 className="text-xl mt-2 font-bold">THE HAUS</h1>
+            <h1 className="text-s">EVENTS</h1>
+          </div>
+          <div className="flex flex-col">
+            <Image src="/images/image00005.jpeg" alt="Image 4" width={200} height={200} className="object-cover h-50 w-50"/>
+            <h1 className="text-xl mt-2 font-bold">THE HAUS</h1>
+            <h1 className="text-s">SOCIAL</h1>
+          </div>
+          <div className="flex flex-col">
+            <Image src="/images/image00005.jpeg" alt="Image 5" width={200} height={200} className="object-cover h-50 w-50"/>
+            <h1 className="text-xl mt-2 font-bold">THE HAUS</h1>
+            <h1 className="text-s">SOCIETY</h1>
           </div>
         </div>
       </div>
